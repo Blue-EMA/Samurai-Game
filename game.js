86607c0444b0c173493a676d9d8318f016f7a1ce
Blue-EMA <146757887+Blue-EMA@users.noreply.github.com>
2024-06-27 14:09:59 -0400

    Update game.js

--- game.js
+++ game.js
@@ -22,20 +22,19 @@ class Example extends Phaser.Scene {
 		this.playerHealth = 100;
     }
 
-    preload() {
+preload() {
         this.load.spritesheet('dude_jump', 'assets/Samurai/Jump.png', { frameWidth: 128, frameHeight: 128 });
         this.load.spritesheet('dude_walk', 'assets/Samurai/Walk.png', { frameWidth: 128, frameHeight: 128 });
         this.load.spritesheet('overhand', 'assets/Samurai/Attack_2.png', { frameWidth: 128, frameHeight: 128 });
         this.load.spritesheet('slash', 'assets/Samurai/Attack_1.png', { frameWidth: 128, frameHeight: 128 });
         
-		this.load.image('ground', 'assets/nature_5/3.png');
+	this.load.image('ground', 'assets/nature_5/3.png');
         this.load.image('sky', 'assets/nature_5/1.png');
         this.load.image('cloud', 'assets/nature_5/2.png');
         this.load.image('field', 'assets/nature_5/4.png');
 		
     }
-	
-	dim() {
+dim() {
     this.dimOverlay = this.add.graphics();
     this.dimOverlay.fillStyle(0x000000, .5);
     this.dimOverlay.fillRect(0, 0, this.sys.game.config.width, this.sys.game.config.height);
@@ -45,27 +44,27 @@ class Example extends Phaser.Scene {
 	}
 	
 
-    create() {
+create() {
         
 		//setting
-		this.add.image(400, 300, 'sky').setScale(1.77);
-		this.cloud = this.add.tileSprite(0, 0, 800, 600, 'cloud').setOrigin(0).setScale(1.75);
+	this.add.image(400, 300, 'sky').setScale(1.77);
+	this.cloud = this.add.tileSprite(0, 0, 800, 600, 'cloud').setOrigin(0).setScale(1.75);
         this.field = this.add.image(400, 300, 'field').setScale(1.85);
         this.ground = this.add.tileSprite(0, 300, 800, 600, 'ground').setOrigin(0).setScale(1);
-		this.dim();
+	this.dim();
 		
 		//player	
         player = this.physics.add.sprite(700, 50, 'dude_walk').setOrigin(0).setScale(1);
         player.setBounce(0.2);
         player.setCollideWorldBounds(true);
         player.setOrigin(0.5, 1);
-		player.flipX = true;
+	player.flipX = true;
 		
 		//enemy
         enemy = this.physics.add.sprite(100, 50, 'dude_walk').setOrigin(0).setScale(1);
         enemy.setBounce(0.2);
         enemy.setCollideWorldBounds(true);
-		enemy.setOrigin(0.5,1);
+	enemy.setOrigin(0.5,1);
 		
 
         // Create health bar
@@ -83,7 +82,7 @@ class Example extends Phaser.Scene {
 		// cursor and key createion extrap.
 		cursors = this.input.keyboard.createCursorKeys();
 		keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-        keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        	keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
 		keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
 		keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
 		keyE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
@@ -91,11 +90,11 @@ class Example extends Phaser.Scene {
 		keyQ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
 		ESC = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
 		keyEnter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
-        keyPeriod = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.PERIOD);
+		keyPeriod = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.PERIOD);
 		keySlash = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.FORWARD_SLASH);
 		
 		// movments 
-		this.anims.create({
+	this.anims.create({
             key: 'left',
             frames: this.anims.generateFrameNumbers('dude_walk', { start: 0, end: 8 }),
             frameRate: 10,
@@ -135,7 +134,7 @@ class Example extends Phaser.Scene {
         this.physics.add.collider(enemy, this.ground);
         this.physics.world.setBounds(0, 0, 800, 600);
 		
-		this.dim();
+	this.dim();
         
 		// Debug graphics
         const graphics = this.add.graphics();
@@ -159,14 +158,14 @@ class Example extends Phaser.Scene {
     update() {
     if (gameOver) {
         return;
-		this.scene.pause();
+	this.scene.pause();
 		
     } else if (ESC.isDown) {
-		this.dimOverlay.setVisible(true);
-		this.scene.pause();
+	this.dimOverlay.setVisible(true);
+	this.scene.pause();
     }
 	this.healthBarGraphics.clear();
-    this.updateHealthBar(100, this.playerHealth);
+    	this.updateHealthBar(100, this.playerHealth);
 	this.updateHealthBar(500, this.enemyHealth);
 	
     this.ground.tilePositionX -= 2;
